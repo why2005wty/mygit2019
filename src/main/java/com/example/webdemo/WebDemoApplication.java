f588c34f1c28498efd5366c50cec4b5292ac696b
why2005wty <why2005wty@163.com>
2019-06-10 17:00:11 +0800

    frist commit

--- src/main/java/com/example/webdemo/WebDemoApplication.java
+++ src/main/java/com/example/webdemo/WebDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebDemoApplication {
 
+    // test commit
     public static void main(String[] args) {
         SpringApplication.run(WebDemoApplication.class, args);
     }
